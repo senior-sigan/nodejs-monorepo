c031a99d40fc042aa647db44f2fe3ae510db3f10
Ilya Siganov <ilya.blan4@gmail.com>
2024-08-27 01:48:40 +0600

    fix bugs

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,6 +1,6 @@
-const makeCoffee = require('@acme/example-old');
+const { makeCoffee } = require('@acme/example-old');
 const { loadAnime } = require('@acme/example-ts');
-const { printToday } = require('@acme/example-new');
+const { printDates } = require('@acme/example-new');
 const Koa = require('koa');
 const app = new Koa();
 
@@ -8,7 +8,7 @@ app.use(async (ctx) => {
   const anime = await loadAnime();
   console.log(anime);
   console.log(makeCoffee());
-  ctx.body = 'Hello World ' + printToday();
+  ctx.body = 'Hello World ' + printDates();
 });
 
 app.listen(3000);
